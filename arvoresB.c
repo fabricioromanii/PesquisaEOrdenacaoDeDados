29b9a0842fbfaf9600533859a2e3173194bd2b81
fabricioromanii <fabricioz.1.2@hotmail.com>
2020-12-25 13:01:51 -0300

    add ultimos conteudos

---
+++ arvoresB.c
@@ -0,0 +1,13 @@
+/*
+Arvores B
+
+Generalizacao das arvores de busca binaria;
+
+-cada nó tem ate N filhos;
+-nós tambem sao chamados e paginas;
+-Armazena ate N-1 chaves de busca por nó, mantendo-as ordenadas
+-auto balanceada
+
+arvore de ordem 3: a raiz temm 3 filhos e os nós tem 3-1 chaves/espaços
+
+*/
---
+++ compressaoDeDados.c
@@ -0,0 +1,15 @@
+/*
+COMPRESSAO DE DADOS
+
+reduzir o espaco de armazenamento;
+a quantidade de dados sempre tende a crescer de mandeira exponencial;
+
+compactar - unir dados;
+comprimir - mudança na representacao de algum dado para reduzir seu tamanho;
+
+codificação de Huffman 
+Desenvolvida por David Huffman durante seu doutorado;
+
+
+
+*/
